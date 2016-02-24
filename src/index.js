d75c64d0f9cad1c1ca7497cc40230d385c763399
Brian McAllister <brian@brianmcallister.com>
2016-02-24 12:24:19 -0500

    First draft.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('hello!')
+const createReducer = (defaultState = {}, reducer) => (state, action) => {
+  let states = {};
+  let handleAction = (type, ...args) => {
+    states[type] = args;
+  }
+  let reduceStates = (state = defaultState, action) => {
+    let actionData = states[action.type];
+
+    if (!actionData) return state;
+
+    // Handle a single 'null' value, which means to reset everything back to default.
+    if (actionData.length === 1 && actionData[0] === null) {
+      return Object.assign({});
+    }
+
+    // Handle function values.
+    actionData = actionData.map((value, index) => {
+      if (typeof value === 'function') {
+        return value(state, action);
+      }
+
+      return value;
+    });
+
+    return Object.assign({}, state, defaultState, ...actionData);
+  }
+
+  reducer(handleAction);
+  return reduceStates;
+}
+
+export default createReducer;
